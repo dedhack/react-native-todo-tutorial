bb6f961d8bb414245a41e081777cb2c2c03dc67d
izharj <eezhar@gmail.com>
2023-03-16 23:50:41 +0800

    basic css for the startscreen

---
+++ mini-game/screens/StartGameScreen.js
@@ -0,0 +1,60 @@
+import { TextInput, Button, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4, // android only property
+    shadowColor: "black", // ios only property
+    shadowOffset: { width: 0, height: 2 }, // ios only property
+    shadowRadius: 6, // ios only property
+    shadowOpacity: 0.25, // ios only property
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+  },
+  numberInput: {
+    height: 50,
+    width: 100,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
